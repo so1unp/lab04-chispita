aef9cc6b4d19b845fd21e753b7662ae6d2ea81d8
brunoguionbajo <001munozbruno@gmail.com>
2026-06-03 17:30:13 -0300

    Código nave v2.5 - movimiento y pantalla (threads 1/2)

--- nave.c
+++ nave.c
@@ -1,6 +1,5 @@
 /*
     COMANDO PARA COMPILAR:
-
     gcc nave.c -lncursesw -o nave
 */
 
@@ -8,91 +7,53 @@
 #include <time.h>
 #include <locale.h>
 #include <stdbool.h>
+#include <pthread.h>
+#include <stdlib.h>
 
-#define corIniX 22 // coordenada x inicial de la nave 
+#define corIniX 21 // coordenada x inicial de la nave 
 #define corIniY 4 // coordenada y inicial de la nave
 
-#define maxCorX 102 // coordenada x máxima para la nave
+#define maxCorX 101 // coordenada x máxima para la nave
 #define maxCorY 24 // coordenada y máxima para la nave
 
 #define columnas 21
 #define filas 11
 
+/*-----VARIABLES INICIALIZADAS-----*/
+time_t ultimoMov;
+char *nave, *modo;
+int x, y, celAlt, celAnch, maxY, maxX, ancho, alto, inicioX, inicioY;
 
-int main() {
-    setlocale(LC_ALL, "");
-    initscr();
-    noecho();
-    curs_set(0); 
-    nodelay(stdscr, TRUE);
+/*SIN LÓGICA APLICADA*/
+int combustible, oxigeno, naves, mutexio, semaforita, kernelio;
+bool modoDisparo;
+/********************/
+/*-----VARIABLES INICIALIZADAS-----*/
 
-    start_color();
-    init_color(8, 200, 200, 200); // gris
-    init_pair(1, 8, COLOR_BLACK);
-    init_pair(2, COLOR_GREEN, COLOR_BLACK);
-    
-    time_t ultimoMov = 0;
-    char *nave = "↓";
-    char *modo = "NAVE";
-    int x = corIniX;
-    int y = corIniY;
-
-    /*SIN LÓGICA APLICADA*/
-    int combustible = 100;
-    int oxigeno = 100;
-    int naves = 1;
-    int mutexio = 0;
-    int semaforita = 0;
-    int kernelio = 0;
-    bool modoDisparo = false;
-    /********************/
-
-    int celAlt = 2; // altura de cada celda
-    int celAnch = 4; // ancho de cada celda
-
-    int maxY, maxX;
-    getmaxyx(stdscr, maxY, maxX);
-    int ancho = columnas * celAnch + 1;
-    int alto  = filas * celAlt + 1;
-    int inicioX = (maxX - ancho) / 2 + 1; // 1 -> desplazamiento hacia la derecha para el mayor centrado
-    int inicioY = (maxY - alto) / 2;
-
-    refresh();
-
-    
-    while (1) {
-        erase();
-
-        time_t relojAhora = time(NULL);
-
-        // Dibujar líneas horizontales
+/*HILO DE INTERFAZ*/
+void *dibujarPantalla(void *arg){
         attron(COLOR_PAIR(1));
-
         for (int i = 0; i <= filas; i++) {
             for (int j = 0; j <= columnas * celAnch; j++) {
                 mvaddch(inicioY + i * celAlt, inicioX + j, '-');
             }
         }
 
-        // Dibujar líneas verticales
         for (int i = 0; i <= filas * celAlt; i++) {
             for (int j = 0; j <= columnas; j++) {
                 mvaddch(inicioY + i, inicioX + j * celAnch, '|');
             }
         }
 
-        // Dibujar intersecciones
         for (int i = 0; i <= filas; i++) {
             for (int j = 0; j <= columnas; j++) {
                 mvaddch(inicioY + i * celAlt, inicioX + j * celAnch, '+');
             }
         }
-        attroff(COLOR_PAIR(1));
+        attroff(COLOR_PAIR(1)); 
 
         attron(COLOR_PAIR(2));
-
         mvprintw(0, 49, ">>>>>>-COSMIKERNEL->>>>>>");
-
         mvaddstr(y, x, nave);
         mvprintw(26, 21, "MODO:%s", modo);
         mvprintw(2, 21, "COMB:%d%%", combustible);
@@ -102,6 +63,64 @@ int main() {
         mvprintw(26, 83, "SEMA:%d", semaforita);
         mvprintw(26, 71, "MUTE:%d", mutexio);
         attroff(COLOR_PAIR(2));
+        pthread_exit(NULL);
+}
+
+int main() {
+    setlocale(LC_ALL, "");
+    initscr();
+    noecho();
+    curs_set(0); 
+    nodelay(stdscr, TRUE);
+
+    pthread_t hilo_pantalla;
+    int resultado;
+
+    start_color();
+    init_color(8, 200, 200, 200); // gris
+    init_pair(1, 8, COLOR_BLACK);
+    init_pair(2, COLOR_GREEN, COLOR_BLACK);
+    
+    ultimoMov = 0;
+    nave = "↓";
+    modo = "NAVE";
+    x = corIniX;
+    y = corIniY;
+
+    /*SIN LÓGICA APLICADA*/
+    combustible = 100;
+    oxigeno = 100;
+    naves = 1;
+    mutexio = 0;
+    semaforita = 0;
+    kernelio = 0;
+    modoDisparo = false;
+    /********************/
+
+    celAlt = 2; // altura de cada celda
+    celAnch = 4; // ancho de cada celda
+
+    getmaxyx(stdscr, maxY, maxX);
+    ancho = columnas * celAnch + 1;
+    alto  = filas * celAlt + 1;
+    inicioX = (maxX - ancho) / 2;
+    inicioY = (maxY - alto) / 2;
+
+    refresh();
+
+    while (1) {
+        erase();
+
+        time_t relojAhora = time(NULL);
+
+        resultado = pthread_create(&hilo_pantalla, NULL, dibujarPantalla, NULL);
+
+        if(resultado!=0){
+            perror("Error al crear el hilo");
+            exit(1);
+        }
+
+        pthread_join(hilo_pantalla, NULL);
 
         refresh();
         int c = getch();
