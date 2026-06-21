8ff337a2898d7b43aff07f972dc27685fa70c3a8
Juanma Acosta <acostajuanma854@gmail.com>
2026-06-21 18:14:12 -0300

    variables compartidas

--- compartido.h
+++ compartido.h
@@ -3,13 +3,11 @@
 
 #include <pthread.h>
 
-// Definición de la cola
-#define COLA_ESTACION "/cola_ypf"
+#define COLA_ESTACION  "/cola_ypf"
+#define COLA_RESPUESTA "/cola_respuesta"
 
-// Estructura de estado compartido
 typedef struct {
     int MAXnaves;
-    int navesEnEstacion;
     int corriendo;
     int oxigeno;
     int nafta;
@@ -17,22 +15,21 @@ typedef struct {
     int mutexio;
     int semaforita;
     int kernelio;
-    
-    // Contadores
     int recolector0;
     int recolector1;
     int oxigeno_jugador;
     int nafta_jugador;
-
-    // Mutexes
     pthread_mutex_t mutex_nafta;
     pthread_mutex_t mutex_oxigeno;
 } EstadoYPF;
 
-// Estructura del mensaje
 typedef struct {
-    int tipo_operacion; // 1: Deuterio, 2: Minerales
-    // Puedes añadir más campos aquí si los necesitas
+    int tipo_operacion; // 1: nafta, 2: oxigeno
 } MensajeNave;
+
+typedef struct {
+    int tipo;        // 1: nafta, 2: oxigeno
+    int cantidad;    // cuánto se recibió (0 si no había stock)
+} RespuestaEstacion;
 
 #endif
